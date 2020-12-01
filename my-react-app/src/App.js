3dcb67f3ce91e9e9c04f68dff7586a9f9bdd77c4
devstrongzhao <384719678@qq.com>
2020-12-01 15:37:02 +0800

    创建create-react-app

---
+++ my-react-app/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const promise = new Promise((resolve, reject) => {
+
+  })
+  return (
+    <div className="App">
+      88
+    </div>
+  );
+}
+
+export default App;
